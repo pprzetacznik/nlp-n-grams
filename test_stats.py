5dd321451454382142903e72fb5a7acd785b3b4d
Piotr Przetacznik <piotr.przetacznik@gmail.com>
2020-11-22 18:20:13 +0100

    Change all files to utf-8

--- test_stats.py
+++ test_stats.py
@@ -1,37 +1,42 @@
-# -*- coding: utf-8 -*-
-#!/usr/bin/env python
-
 import unittest
-import numpy as np
-from stats import cosine_distance, initialize_n_grams_list, clean, get_n_grams_from_string, count_n_grams, count_stat_for_document
+from stats import (
+    cosine_distance,
+    initialize_n_grams_list,
+    clean,
+    get_n_grams_from_string,
+    count_n_grams,
+    count_stat_for_document,
+)
 
-class TestLogger(unittest.TestCase):
-  def setUp(self):
-    pass
 
-  def tearDown(self):
-    pass
+class TestStats(unittest.TestCase):
+    def setUp(self):
+        pass
 
-  def test_cosine_distance(self):
-    print cosine_distance([1,2,3], [2,3,4])
-    self.assertAlmostEqual(cosine_distance([3,4,5], [3,4,5]), 1)
-    self.assertEqual(cosine_distance([1,2], [-2,1]), 0)
+    def tearDown(self):
+        pass
 
-  def test_initialize_n_grams_list(self):
-    print initialize_n_grams_list(1)
-    # print initialize_n_grams_list(2)
+    def test_cosine_distance(self):
+        print(cosine_distance([1, 2, 3], [2, 3, 4]))
+        self.assertAlmostEqual(cosine_distance([3, 4, 5], [3, 4, 5]), 1)
+        self.assertEqual(cosine_distance([1, 2], [-2, 1]), 0)
 
-  def test_clean(self):
-    print clean("abecadło z pieca spadło123")
+    def test_initialize_n_grams_list(self):
+        print(initialize_n_grams_list(1))
+        print(initialize_n_grams_list(2))
 
-  def test_get_n_grams_from_string(self):
-    print get_n_grams_from_string("string", 3)
+    def test_clean(self):
+        print(clean("abecadło z pieca spadło123"))
 
-  def test_count_n_grams(self):
-    print count_n_grams("to jest testowy tekst".split(), 3)
+    def test_get_n_grams_from_string(self):
+        print(get_n_grams_from_string("string", 3))
 
-  def test_count_stat_for_document(self):
-    print count_stat_for_document('train_corpus/polski.txt', 3)
+    def test_count_n_grams(self):
+        print(count_n_grams("to jest testowy tekst".split(), 3))
 
-if __name__ == '__main__':
-  unittest.main()
+    def test_count_stat_for_document(self):
+        print(count_stat_for_document("train_corpus/polski.txt", 3))
+
+
+if __name__ == "__main__":
+    unittest.main()
